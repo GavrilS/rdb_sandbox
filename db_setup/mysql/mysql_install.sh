c9eb22eb5bfd5ffa4f517589fcdb7837c74f2f5e
GavrilS <gavr1lll@abv.bg>
2024-11-13 18:31:01 +0200

    Separate mysql installation/start procedures in separate files

--- db_setup/mysql/mysql_install.sh
+++ db_setup/mysql/mysql_install.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
 sudo apt install mysql-server
-
-echo "==========================="
-echo "Check the status of the MySQL service"
-sudo service mysql status
-
-echo "==========================="
-echo "Check the network status of the MySQL service"
-sudo ss -tap | grep mysql
-
-# If the service is not running properly we can restart it with the following command
-# sudo service mysql restart
-
-# For troubleshooting problems we can use the systemd journal
-# sudo journalctl -u mysql
---
+++ db_setup/mysql/start_mysql.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo "Starting the MySQL service"
+sudo service mysql start
+echo "==========================="
+
+echo "Check the status of the MySQL service"
+sudo service mysql status
+echo "==========================="
+
+echo "Check the network status of the MySQL service"
+sudo ss -tap | grep mysql
+echo "==========================="
+
+# If the service is not running properly we can restart it with the following command
+# sudo service mysql restart
+
+# For troubleshooting problems we can use the systemd journal
+# sudo journalctl -u mysql
